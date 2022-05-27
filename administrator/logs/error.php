f5fa63cf9f4c28f4b1c1f250a9762700d47a889a
nam.luong <nam.luong@invitae.com>
2022-05-27 14:55:00 +0700

    add example component

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2022-05-24 08:25:28 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2022-05-24T08:25:28+00:00	INFO 127.0.0.1	cookiefailure	Empty password not allowed.
